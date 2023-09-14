dfa4414b1d01a715b60db2c4eb29d5cd87ae499b
Suru-Suraj <118275588+Suru-Suraj@users.noreply.github.com>
2023-09-14 22:10:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
 resource "aws_key_pair" "suraj" {
   key_name   = "suraj"
-  public_key = file("suraj.pem")
+  public_key = tls_private_key.CAPSTONE.public_key_openssh
+}
+resource "tls_private_key" "CAPSTONE" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+resource "local_file" "CAPSTONE" {
+  content  = tls_private_key.CAPSTONE.private_key_pem
+  filename = "capstone.pem"
 }
