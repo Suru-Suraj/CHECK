cb57de18643fd2e3fe2e0c40c063ecd5a3d55458
Suru-Suraj <118275588+Suru-Suraj@users.noreply.github.com>
2023-09-15 12:41:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -239,5 +239,5 @@ resource "aws_autoscaling_group" "CAPSTONE" {
 # Attachement of autoscaling groups and target groups
 resource "aws_autoscaling_attachment" "example" {
   autoscaling_group_name = aws_autoscaling_group.CAPSTONE.id
-  alb_target_group_arn    = aws_lb_target_group.CASPTONE.arn
+  lb_target_group_arn    = aws_lb_target_group.CASPTONE.arn
 }
