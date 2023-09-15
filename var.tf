6c5df344fc5cee648a3dc7773c8d69b6a41141b9
Suru-Suraj <118275588+Suru-Suraj@users.noreply.github.com>
2023-09-15 11:14:22 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,45 +1,51 @@
-# VPC Settings
+# AWS Region
+variable "aws_region" {
+  description = "The AWS region where resources will be created."
+  default     = "us-east-1"
+}
+
+# VPC CIDR Block
 variable "vpc_cidr_block" {
-  description = "CIDR block for the VPC"
-  type        = string
+  description = "The CIDR block for the VPC."
   default     = "10.0.0.0/16"
 }
 
-# Subnet Settings
-variable "subnet_cidr_block" {
-  description = "CIDR block for the public subnet"
-  type        = string
-  default     = "10.0.1.0/24"
+# Subnet CIDR Blocks
+variable "subnet_cidr_blocks" {
+  description = "A map of subnet CIDR blocks."
+  type        = map(string)
+  default = {
+    public1 = "10.0.1.0/24"
+    public2 = "10.0.2.0/24"
+    private = "10.0.3.0/24"
+  }
 }
 
-variable "subnet_availability_zone" {
-  description = "Availability Zone for the public subnet"
-  type        = string
-  default     = "us-east-1a"
+# Availability Zones
+variable "availability_zones" {
+  description = "A list of availability zones for subnets."
+  type        = list(string)
+  default = [
+    "us-east-1a",
+    "us-east-1b",
+    "us-east-1c",
+  ]
 }
 
-# Security Group Settings
-variable "allowed_ports" {
-  description = "List of allowed ports for the security group"
-  type        = list(number)
-  default     = [22, 80, 443, 3000]
+# SSH Key Name
+variable "ssh_key_name" {
+  description = "The name of the SSH key pair for instances."
+  default     = "suru"
 }
 
-# EC2 Instance Settings
-variable "instance_ami" {
-  description = "AMI ID for the EC2 instance"
-  type        = string
-  default     = "ami-053b0d53c279acc90"
-}
-
+# Instance Type
 variable "instance_type" {
-  description = "Instance type for the EC2 instance"
-  type        = string
+  description = "The EC2 instance type."
   default     = "t2.micro"
 }
 
-variable "instance_key_name" {
-  description = "Name of the EC2 key pair"
-  type        = string
-  default     = "capstone"
+# AMI ID
+variable "ami_id" {
+  description = "The ID of the Amazon Machine Image (AMI)."
+  default     = "ami-06a0a61d43cf06546"
 }
